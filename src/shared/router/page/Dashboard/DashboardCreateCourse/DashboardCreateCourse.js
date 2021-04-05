d7043ddf750e1b7577b6a48691b9fb4dc234fe09
SorninL <sornin.contact@gmail.com>
2021-04-05 18:41:18 +0200

    Started the create course component, created usefull componenent and a lot of UI utils

--- src/shared/router/page/Dashboard/DashboardCreateCourse/DashboardCreateCourse.js
+++ src/shared/router/page/Dashboard/DashboardCreateCourse/DashboardCreateCourse.js
@@ -1,6 +1,9 @@
 import './DashboardCreateCourse.css';
 import React from "react";
 import {useHistory} from "react-router-dom";
+import ProgressBar from "../../../../ui/ProgressBar/ProgressBar";
+import DashboardCreateCourseQuestions from "./CourseCreateQuestions/CourseCreateQuestions";
+import CoursePreview from "../../../../ui/CoursePreview/CoursePreview";
 
 function DashboardCreateCourse() {
   const history = useHistory();
@@ -8,11 +11,29 @@ function DashboardCreateCourse() {
 }
 
 class DashboardCreateCourseComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 0
+    }
+  }
 
   render() {
     return (
       <div className="dashboardCreateCourse-root">
-        CreateCourse
+        <div className="dashboardCreateCourse-centerFix">
+          <div className="dashboardCreateCourse-mainTitle">Bienvenue sur la création de cours</div>
+          <div className="dashboardCreateCourse-subTitle">C'est ici que tout commence, répondez à ce questionnaire pour débuter</div>
+          <div className="dashboardCreateCourse-progressBar">
+            <ProgressBar progressType="success" barSize={600} progressSize={10} text="Titre - 1"/>
+          </div>
+          <div className="dashboardCreateCourse-questions">
+            <DashboardCreateCourseQuestions />
+          </div>
+          <div className="dashboardCreateCourse-preview">
+            <CoursePreview courseImg="/src" courseTitle="Fabriquez votre fauteuil de jardin en palettes" courseText="Tutoriel de construction d'un fauteuil de jardin avec des palettes en bois" courseDifficulty={3}/>
+          </div>
+        </div>
       </div>
     )
   }
